2d83145b9d838f2ae241c6819e7fd39d0045fc77
Abul Kawser <uiu.afser@gmail.com>
2022-11-27 20:08:19 +0600

    jwt verify admin type of user save user to db added

--- index.js
+++ index.js
@@ -2,14 +2,79 @@ const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
 const port = process.env.PORT || 5000;
 const app = express();
 
+//DB_PASS=ICRw1DNG3tOhHcYl
+//DB_USER=becheFel
 app.use(cors());
 app.use(express.json());
 
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gt8bmu9.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+
+    const usersCollection = client.db('becheFel').collection('users');
+
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user.role !== "admin") {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+
+      next();
+    };
+
+    app.get("/jwt", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user) {
+        const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, {
+          expiresIn: "12h",
+        });
+        return res.send({ accessToken: token });
+      }
+      res.status(403).send({ accessToken: "" });
+    });
+
+    //add users
+       app.post("/users", async (req, res) => {
+         const user = req.body;
+         const result = await usersCollection.insertOne(user);
+         res.send(result);
+       });
+    //show all users
+
+
+    //send user role
+      app.get("/users/role/:email", async (req, res) => {
+        const email = req.params.email;
+        const query = { email: email };
+        const user = await usersCollection.findOne(query);
+        res.send({ role: user?.role });
+      });
+  
+
+  } finally {
+  }
+}
+
+run().catch(console.log());
 
 
 app.get("/", async (req, res) => {
@@ -18,3 +83,5 @@ app.get("/", async (req, res) => {
 app.listen(port, () => {
   console.log(`Beche Fel is running on ${port}`);
 });
+
+
